eadc21eb3c070636c15b682cb03e051efde9429e
Diego-AA <114123683+Diego-AA@users.noreply.github.com>
2023-01-23 01:43:52 -0300

    Add files via upload

---
+++ cv_Ticmas/js/funcion.js
@@ -0,0 +1,11 @@
+
+document.getElementById('boton1').onclick = function () {
+    console.log("hiciste clic");
+    document.getElementById("expe").innerHTML = "Responsable de las actualizaciones periódicas de la página principal de Seguros Amerita. Desarrollé una plataforma de e-commerce que elevó las ventas online en un 15%. Mis diseños elevaron el tiempo de permanencia de los usuarios en un 5%.Diseñé más de 200 imágenes promocionales que fueron utilizadas tanto en el sitio web como en otras plataformas de comunicación digital de la empresa.";
+}
+
+
+document.getElementById('imagen').onclick = function () {
+    console.log("hiciste clic");
+    document.getElementById("elemento").style.display = "block";
+    }
